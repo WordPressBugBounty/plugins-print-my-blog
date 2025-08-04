b73dd8ab04a9c71a10f2db92221fc384eed4054f
Sean Murphy <sean@iamseanmurphy.com>
2025-08-04 20:19:36 +0000

    3.27.10

--- print-my-blog/printmyblog.php
+++ print-my-blog/printmyblog.php
@@ -10,7 +10,7 @@
  * Description: Make printing your blog easy and impressive. For you & your visitors. One post or thousands.
  * Author: Michael Nelson
  * Author URI: https://printmy.blog
- * Version: 3.27.9
+ * Version: 3.27.10
  * Requires at least: 4.7
  * Requires PHP: 5.4
  * Text Domain: print-my-blog
@@ -38,7 +38,7 @@ if ( defined( 'PMB_VERSION' ) ) {
         ?>
             </p>
         </div>
-        <?php 
+    <?php 
     }
 
     add_action(
@@ -70,7 +70,7 @@ if ( defined( 'PMB_VERSION' ) ) {
         ?>
             </p>
         </div>
-        <?php 
+    <?php 
     }
 
     add_action(
@@ -108,7 +108,7 @@ if ( defined( 'PMB_VERSION' ) ) {
         ?>
             </p>
         </div>
-        <?php 
+<?php 
     }
 
     add_action(
@@ -119,7 +119,7 @@ if ( defined( 'PMB_VERSION' ) ) {
     );
 } else {
     // it's all good! start bootstraping PMB.
-    define( 'PMB_VERSION', '3.27.9' );
+    define( 'PMB_VERSION', '3.27.10' );
     define( 'PMB_DIR', wp_normalize_path( __DIR__ ) . '/' );
     define( 'PMB_MAIN_FILE', __FILE__ );
     define( 'PMB_TEMPLATES_DIR', PMB_DIR . 'templates/' );
